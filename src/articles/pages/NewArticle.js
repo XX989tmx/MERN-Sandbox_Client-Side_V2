28a3befde9177c25039a50f507743592ab0b6df0
XX989tmx <issacgravity8@gmail.com>
2020-06-21 15:45:17 +0900

    input form

--- src/articles/pages/NewArticle.js
+++ src/articles/pages/NewArticle.js
@@ -1,21 +1,102 @@
-import React from 'react';
+import React, { useCallback, useReducer } from "react";
 
-import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
-import './NewArticle.css';
-import Input from '../../shared/components/FormElements/Input';
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+import "./NewArticle.css";
+import Input from "../../shared/components/FormElements/Input";
+import Button from '../../shared/components/FormElements/Button';
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
+    default:
+      return state;
+  }
+};
 
 const NewArticle = () => {
-    return (
-      <form className="article-form">
-        <Input
-          element="input"
-          type="text"
-          label="Title"
-          placeholder="text"
-          validators={[VALIDATOR_REQUIRE()]}
-          errorText="Please enter a valid title."
-        />
-        <Input element="input" type="radio" label="radio" placeholder="radio" />
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      post: {
+        value: "",
+        isValid: false,
+      },
+      author: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: "INPUT_CHANGE",
+      value: value,
+      isValid: isValid,
+      inputId: id,
+    });
+  }, []);
+
+  const placeSubmitHandler = (event) => {
+      event.preventDefault();
+      console.log(formState.inputs);
+      
+  }
+
+  return (
+    <form className="article-form" onSubmit={placeSubmitHandler}>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        placeholder="text"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={inputHandler}
+      />
+      <Input
+        id="post"
+        element="textarea"
+        label="Post"
+        placeholder="Write your post here"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid post (at least 5 characters)."
+        onInput={inputHandler}
+      />
+      <Input
+        id="author"
+        element="input"
+        label="Author"
+        placeholder="Your name"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid Name."
+        onInput={inputHandler}
+      />
+      {/* <Input element="input" type="radio" label="radio" placeholder="radio" />
         <Input element="input" type="email" label="email" placeholder="email" />
         <Input
           element="input"
@@ -28,9 +109,12 @@ const NewArticle = () => {
           type="checkbox"
           label="checkbox"
           placeholder="checkbox"
-        />
-      </form>
-    );
+        /> */}
+      <Button type="submit" disabled={!formState.isValid}>
+        ADD ARTICLE
+      </Button>
+    </form>
+  );
 };
 
 export default NewArticle;
