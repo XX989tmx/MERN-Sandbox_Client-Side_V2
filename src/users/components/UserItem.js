2d2cc4ded30a16b5720af7b9c3d43f1c59fac143
XX989tmx <issacgravity8@gmail.com>
2020-06-30 00:15:26 +0900

    get request for users, added avatar

--- src/users/components/UserItem.js
+++ src/users/components/UserItem.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Card from "../../shared/components/UIElements/Card";
+import Avatar from "../../shared/components/UIElements/Avatar";
 
-const UserItem = () => {
+const UserItem = (props) => {
   return (
-      <div>Users</div>
+    <li className="user-item">
+      <Card className="user-item__content">
+        <Link to={`/${props.id}/articles`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.articleCount}
+              {props.articleCount === 1 ? "Article" : "Articles"}
+            </h3>
+          </div>
+        </Link>
+      </Card>
+    </li>
   );
 };
 
--- src/users/components/UsersList.js
+++ src/users/components/UsersList.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import UserItem from "../components/UserItem";
+import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
 
-const UsersList = () => {
-    return <UserItem />
+const UsersList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
+      </div>
+    )
+  }
+
+  return (
+    <ul className="users-list">
+      {props.items.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          articleCount={user.articles.length}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default UsersList;
