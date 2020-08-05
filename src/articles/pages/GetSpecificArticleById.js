24119a9da431ee1b7197206860008b42f3b129a4
XX989tmx <issacgravity8@gmail.com>
2020-08-06 03:28:19 +0900

    adds loadingSpinner to GetSpesificArticleById.js

--- src/articles/pages/GetSpecificArticleById.js
+++ src/articles/pages/GetSpecificArticleById.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { useHttpClient } from "../../shared/hooks/http-hook";
 import ArticleItem from "../components/ArticleItem";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const GetSpecificArticleById = () => {
 const { isLoading, error, sendRequest, clearError } = useHttpClient();
@@ -18,31 +19,38 @@ useEffect(() => {
       console.log(responseData);
       setArticle(responseData.article);
     } catch (error) {}
-    window.scrollTo(0, 0);
+    // window.scrollTo(0, 0);
   };
   getArticleById();
 }, [sendRequest]);
 
 
     return (
-      <div className="container">
-        <div className="main-container">
-          <div>
-            <ArticleItem
-              key={Article.id}
-              id={Article.id}
-              image={Article.image}
-              title={Article.title}
-              content={Article.content}
-              author={Article.author}
-              categories={Article.categories}
-              tags={Article.tags}
-              date_created={Article.date_created}
-            />
+      <React.Fragment>
+        {isLoading && (
+          <div className="center">
+            <LoadingSpinner />
           </div>
-        </div>
-        <div className="side-container"></div>
-      </div>
+        )}
+        {!isLoading && Article && <div className="container">
+          <div className="main-container">
+            <div>
+              <ArticleItem
+                key={Article.id}
+                id={Article.id}
+                image={Article.image}
+                title={Article.title}
+                content={Article.content}
+                author={Article.author}
+                categories={Article.categories}
+                tags={Article.tags}
+                date_created={Article.date_created}
+              />
+            </div>
+          </div>
+          <div className="side-container"></div>
+        </div>}
+      </React.Fragment>
     );
 }
 
