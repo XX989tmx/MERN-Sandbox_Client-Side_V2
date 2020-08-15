e08ad7366b4f3009988d606b74b5e81d1892f04a
XX989tmx <issacgravity8@gmail.com>
2020-08-15 10:57:58 +0900

    style change in avatart & userItem

--- src/shared/components/UIElements/Avatar.js
+++ src/shared/components/UIElements/Avatar.js
@@ -5,7 +5,7 @@ import './Avatar.css';
 const Avatar = props => {
   return (
     <div className={`avatar ${props.className}`} style={props.style}>
-      <img
+      <img 
         src={props.image}
         alt={props.alt}
         style={{ width: props.width, height: props.width }}
