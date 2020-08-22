24d6a887ef2603738f1b1f8ae37af9f21380d4e3
XX989tmx <issacgravity8@gmail.com>
2020-08-22 17:59:45 +0900

    adds material UI

--- src/shared/components/UIElements/MoveToTopButton.js
+++ src/shared/components/UIElements/MoveToTopButton.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 
 import './MoveToTopButton.css';
 
@@ -6,7 +7,7 @@ const MoveToTopButton = () => {
   const moveToTopButton = (params) => {
     window.scrollTo(0, 0);
   }
-  return <div className="move-to-top-button" onClick={moveToTopButton}></div>;
+  return <div className="move-to-top-button" onClick={moveToTopButton}><ExpandLessIcon style={{color:'white', position:'relative',top:'19px',left:'19px'}}/></div>;
 }
 
 export default MoveToTopButton;
