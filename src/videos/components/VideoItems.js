c9e901f7b84ee3b3afe96e0160d5e219d197bfb6
XX989tmx <issacgravity8@gmail.com>
2020-09-07 10:23:36 +0900

    fix flex property of video item

--- src/videos/components/VideoItems.js
+++ src/videos/components/VideoItems.js
@@ -5,14 +5,14 @@ import "./VideoItems.css";
 
 const VideoItems = (props) => {
   return (
-    <li className="video-item-container center">
+    <li className="video-item-container">
       <Link to={`/videos/${props.id}`} style={{ textDecoration: "none" }}>
         <div className="video-image-box">
           <img
             // src="https://storage.googleapis.com/sample_test_image_bucket/images/2020-06-10%20147.jpg"
             src={props.image}
             alt=""
-            style={{ width: "270px", height: "160px" }}
+            style={{ width: "220px", height: "125px" }}
           />
           {props.hd && <span className="hd">HD</span>}
           {props.is4k && <span className="is4K">4K</span>}
