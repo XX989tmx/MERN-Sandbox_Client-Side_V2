c818abefe47dce295378199d927b8878f59cae4b
XX989tmx <issacgravity8@gmail.com>
2020-09-26 12:18:40 +0900

    adds css styling to crypto index

--- src/cryptos/components/CryptoIndexItem.js
+++ src/cryptos/components/CryptoIndexItem.js
@@ -10,28 +10,29 @@ const CryptoIndexItem = (props) => {
           <span></span>
         </td>
         <td className="td-crypto-index-item2">
-          <span>{props.marketRank}</span>
+          <span className="market-rank">{props.marketRank}</span>
         </td>
         <td className="td-crypto-index-item3">
           <span>
             <img className="crypto-icon" src={props.icon} alt="" />
           </span>{" "}
-          <span>{props.name}</span> <span>{props.code}</span>
+          <span className="coin-name">{props.name}</span>{" "}
+          <span className="currency-code">{props.code}</span>
         </td>
         <td className="td-crypto-index-item4">
-          <span>{props.marketCap}</span>
+          <span className="market-cap">{props.marketCap}</span>
         </td>
         <td className="td-crypto-index-item5">
-          <span>{props.price}</span>
+          <span className="coin-price">{props.price}</span>
         </td>
         <td className="td-crypto-index-item6">
-          <span>0.00%</span>
+          <span className="a24hour-change">0.00%</span>
         </td>
         <td className="td-crypto-index-item7">
-          <span>{props.a24hourVolume}</span>
+          <span className="a24hour-volume">{props.a24hourVolume}</span>
         </td>
         <td className="td-crypto-index-item8">
-          <span>{props.circulatingSupply}</span>
+          <span className="circulating-supply">{props.circulatingSupply}</span>
         </td>
         <td className="td-crypto-index-item9">
           <span>Price Graph</span>
