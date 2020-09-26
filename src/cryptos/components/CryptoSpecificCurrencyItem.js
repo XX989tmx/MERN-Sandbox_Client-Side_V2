be1a4c03cf271d2a8afc1285c26bee95c7a81f9e
XX989tmx <issacgravity8@gmail.com>
2020-09-26 13:33:14 +0900

    adds modified structure to crypto specific currency

--- src/cryptos/components/CryptoSpecificCurrencyItem.js
+++ src/cryptos/components/CryptoSpecificCurrencyItem.js
@@ -4,112 +4,173 @@ const CryptoSpecificCurrencyItem = (props) => {
   return (
     <div className="crypto-specific-item-main-container">
       <div>
+        {/* upper-area1 */}
         <div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div>
+            <div>
+              <div>
+                <img src={props.icon} alt="" />
+              </div>
+              <div>
+                <h1>{props.name}</h1>
+                {props.code}
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <span>{props.price}</span>
+              <span>{props.roi}</span>
+            </div>
+            <div>
+              <span>profit?%</span>
+            </div>
+            <div>
+              <button>share</button>
+            </div>
+          </div>
+          <div>
+            <div>
+              <button>Buy</button>
+              <button>Exchange</button>
+              <button>Gamble</button>
+              <button>Earn Crypto</button>
+            </div>
+            <div>sponsored</div>
+          </div>
         </div>
+        {/* upper-area-2 */}
         <div>
-          <div></div>
-          <div></div>
+          {/* link list */}
+          <div>
+            <ul>
+              <li>Rank {props.marketRank}</li>
+              <li>Website{props.website}</li>
+              <li>Announcement</li>
+              <li>Explorer</li>
+              <li>Message Board</li>
+              <li>Chat</li>
+              <li>Source Code{props.sourceCode}</li>
+              <li>Technical Documentation{props.technicalDocumentation}</li>
+              <li>
+                <button>Coin</button>
+                <button>Inactive</button>
+              </li>
+            </ul>
+          </div>
+          {/* marketcap/volume/circulating supply */}
+          <div>
+            <tr>
+              <th>Market Cap</th>
+              <th>Volume (24h)</th>
+              <th>Circulating Supply</th>
+            </tr>
+            <tr>
+              <td>{props.marketCap}</td>
+              <td>{props.a24hourVolume}</td>
+              <td>{props.circulatingSupply}</td>
+            </tr>
+          </div>
         </div>
       </div>
-      <div>nav</div>
-      <div>charts</div>
+      <div>
+        <span>Charts</span>
+        <span>Market Pairs</span>
+        <span>Social</span>
+        <span>Tools</span>
+        <span>Historical Data</span>
+        <span>Ratings</span>
+        <span>On-Chain Analysis</span>
+        <span>News</span>
+      </div>
+      <div>{props.name} Charts</div>
       <div>
         <div>
           <h1>articles</h1>
         </div>
         <div>
           <h1>tables</h1>
-          <table><tr>
+          <table>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <th></th>
               <td></td>
-          </tr></table>
+            </tr>
+          </table>
         </div>
       </div>
 
-      <img src={props.icon} alt="" />
-      <h1>{props.name}</h1>
-      {props.code}
-      {props.price}
-      {props.roi}
-      {props.marketRank}
-      {props.marketCap}
-      {props.a24hourVolume}
-      {props.circulatingSupply}
       {props.totalSupply}
       {props.maxSupply}
       {props.allTimeHigh}
@@ -124,8 +185,6 @@ const CryptoSpecificCurrencyItem = (props) => {
       {props.yesterdaysChange}
       {props.yesterdaysVolume}
       {props.website}
-      {props.sourceCode}
-      {props.technicalDocumentation}
 
       {props.tokenInsightRating}
       {props.dynamicPerformanceScore}
