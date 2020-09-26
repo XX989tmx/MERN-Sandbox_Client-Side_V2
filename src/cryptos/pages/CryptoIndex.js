28376d4444d2495356da87e35f34d6d97df0d385
XX989tmx <issacgravity8@gmail.com>
2020-09-26 18:02:08 +0900

    adds style to crypto index

--- src/cryptos/pages/CryptoIndex.js
+++ src/cryptos/pages/CryptoIndex.js
@@ -18,46 +18,48 @@ const CryptoIndex = () => {
   }, []);
 
   return (
-    <div>
-      <h2>Top 100 Cryptocurrencies by Market Capitalization</h2>
-      {/* <table className="thead-tag">
+    <div className="crypto-index-container">
+      <div>
+        <h2>Top 100 Cryptocurrencies by Market Capitalization</h2>
+        {/* <table className="thead-tag">
         <thead className="thead-crypto-index"> */}
-          <tr className="tr-table-header">
-            <th className="table-header-header1">
-              <span></span>
-            </th>
-            <th className="table-header-header2">
-              <span>Rank</span>
-            </th>
-            <th className="table-header-header3">
-              <span>Name</span>
-            </th>
-            <th className="table-header-header4">
-              <span>Market Cap</span>
-            </th>
-            <th className="table-header-header5">
-              <span>Price</span>
-            </th>
-            <th className="table-header-header6">
-              <span>Change(24h)</span>
-            </th>
-            <th className="table-header-header7">
-              <span>Volume(24h)</span>
-            </th>
-            <th className="table-header-header8">
-              <span>Circulating Supply</span>
-            </th>
-            <th className="table-header-header9">
-              <span>Price Graph</span>
-            </th>
-            <th className="table-header-header10">
-              <span></span>
-            </th>
-          </tr>
+        <tr className="tr-table-header">
+          <th className="table-header-header1">
+            <span></span>
+          </th>
+          <th className="table-header-header2">
+            <span>Rank</span>
+          </th>
+          <th className="table-header-header3">
+            <span>Name</span>
+          </th>
+          <th className="table-header-header4">
+            <span>Market Cap</span>
+          </th>
+          <th className="table-header-header5">
+            <span>Price</span>
+          </th>
+          <th className="table-header-header6">
+            <span>Change(24h)</span>
+          </th>
+          <th className="table-header-header7">
+            <span>Volume(24h)</span>
+          </th>
+          <th className="table-header-header8">
+            <span>Circulating Supply</span>
+          </th>
+          <th className="table-header-header9">
+            <span>Price Graph</span>
+          </th>
+          <th className="table-header-header10">
+            <span></span>
+          </th>
+        </tr>
         {/* </thead> */}
 
         <CryptoIndexList CryptoArray={CryptoArray} />
-      {/* </table> */}
+        {/* </table> */}
+      </div>
     </div>
   );
 };
