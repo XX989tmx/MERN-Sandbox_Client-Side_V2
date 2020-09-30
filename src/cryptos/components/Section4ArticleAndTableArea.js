c67ca2853bf64462b2aa9b629a26d6d7797646ae
XX989tmx <issacgravity8@gmail.com>
2020-09-30 22:55:33 +0900

    adds conditional rendering to table area

--- src/cryptos/components/Section4ArticleAndTableArea.js
+++ src/cryptos/components/Section4ArticleAndTableArea.js
@@ -2,10 +2,63 @@ import React, { useEffect, useState } from "react";
 import { useParams, useHistory, Link } from "react-router-dom";
 
 const Section4ArticleAndTableArea = (props) => {
+  // for conditional rendering. isData ? data : no data
+
+  const [Name, setName] = useState();
+  const [ROI, setROI] = useState();
+  const [MarketRank, setMarketRank] = useState();
+  const [MarketCapUSD, setMarketCapUSD] = useState();
+  const [a24HourVolumeUSD, seta24HourVolumeUSD] = useState();
+  const [CirculatingSupply, setCirculatingSupply] = useState();
+  const [TotalSupply, setTotalSupply] = useState();
+
   const [MaxSupply, setMaxSupply] = useState();
+  const [AllTimeHigh, setAllTimeHigh] = useState();
+  const [AllTimeLow, setAllTimeLow] = useState();
+  const [a52WeekHigh, seta52WeekHigh] = useState();
+  const [a52WeekLow, seta52WeekLow] = useState();
+  const [a90DayHigh, seta90DayHigh] = useState();
+  const [a90DayLow, seta90DayLow] = useState();
+  const [a30DayHigh, seta30DayHigh] = useState();
+  const [a30DayLow, seta30DayLow] = useState();
+  const [a7DayHigh, seta7DayHigh] = useState();
+  const [a7DayLow, seta7DayLow] = useState();
+  const [a24HourHigh, seta24HourHigh] = useState();
+  const [a24HourLow, seta24HourLow] = useState();
+  const [YesterdaysHigh, setYesterdaysHigh] = useState();
+  const [YesterdaysLow, setYesterdaysLow] = useState();
+  const [YesterdaysOpen, setYesterdaysOpen] = useState();
+  const [YesterdaysClose, setYesterdaysClose] = useState();
+  const [YesterdaysChange, setYesterdaysChange] = useState();
+  const [YesterdaysVolume, setYesterdaysVolume] = useState();
+
   useEffect(() => {
     const onLoad = (params) => {
+      setROI(props.roi);
+      setMarketRank(props.marketRank);
+      setMarketCapUSD(props.marketCap.usd);
+      seta24HourVolumeUSD(props.a24hourVolume.usd);
+      setCirculatingSupply(props.circulatingSupply);
+      setTotalSupply(props.totalSupply);
       setMaxSupply(props.maxSupply);
+      setAllTimeHigh(props.allTimeHigh);
+      setAllTimeLow(props.allTimeLow);
+      seta52WeekHigh(props.a52weekHigh);
+      seta52WeekLow(props.a52weekLow);
+      seta90DayHigh(props.a90dayHigh);
+      seta90DayLow(props.a90dayLow);
+      seta30DayHigh(props.a30dayHigh);
+      seta30DayLow(props.a30dayLow);
+      seta7DayHigh(props.a7dayHigh);
+      seta7DayLow(props.a7dayLow);
+      seta24HourHigh(props.a24hourHigh);
+      seta24HourLow(props.a24hourLow);
+      setYesterdaysHigh(props.yesterdaysHigh);
+      setYesterdaysLow(props.yesterdaysLow);
+      setYesterdaysOpen(props.yesterdaysOpen);
+      setYesterdaysClose(props.yesterdaysClose);
+      setYesterdaysChange(props.yesterdaysChange);
+      setYesterdaysVolume(props.yesterdaysVolume);
     };
     onLoad();
   }, []);
@@ -78,117 +131,187 @@ const Section4ArticleAndTableArea = (props) => {
           </tr>
           <tr className="table-row-crypto-specific">
             <th className="table-header-crypto-specific">{props.name} ROI</th>
-            <td className="table-data-crypto-specific">{props.roi}</td>
+            {ROI ? (
+              <td className="table-data-crypto-specific">{props.roi}</td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
           <tr className="table-row-crypto-specific grey-color-row">
             <th className="table-header-crypto-specific">Market Rank</th>
-            <td className="table-data-crypto-specific">{props.marketRank}</td>
+            {MarketRank ? (
+              <td className="table-data-crypto-specific">{props.marketRank}</td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
           <tr className="table-row-crypto-specific">
             <th className="table-header-crypto-specific">Market Cap</th>
-            <td className="table-data-crypto-specific">
-              $ {props.marketCap.usd} USD
-            </td>
+            {MarketCapUSD ? (
+              <td className="table-data-crypto-specific">
+                $ {props.marketCap.usd} USD
+              </td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
           <tr className="table-row-crypto-specific grey-color-row">
             <th className="table-header-crypto-specific">24 Hour Volume</th>
-            <td className="table-data-crypto-specific">
-              $ {props.a24hourVolume.usd} USD
-            </td>
+            {a24HourVolumeUSD ? (
+              <td className="table-data-crypto-specific">
+                $ {props.a24hourVolume.usd} USD
+              </td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
           <tr className="table-row-crypto-specific">
             <th className="table-header-crypto-specific">Circulating Supply</th>
-            <td className="table-data-crypto-specific">
-              {props.circulatingSupply} {props.code}
-            </td>
+            {CirculatingSupply ? (
+              <td className="table-data-crypto-specific">
+                {props.circulatingSupply} {props.code}
+              </td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
           <tr className="table-row-crypto-specific grey-color-row">
             <th className="table-header-crypto-specific">Total Supply</th>
-            <td className="table-data-crypto-specific">
-              {props.totalSupply} {props.code}
-            </td>
+            {TotalSupply ? (
+              <td className="table-data-crypto-specific">
+                {props.totalSupply} {props.code}
+              </td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
           <tr className="table-row-crypto-specific">
             <th className="table-header-crypto-specific">Max Supply</th>
-            {MaxSupply ? <td className="table-data-crypto-specific">
-               {props.maxSupply} {props.code} 
-            </td> : <td className="table-data-crypto-specific">
-               No Data
-            </td> }
+            {MaxSupply ? (
+              <td className="table-data-crypto-specific">
+                {props.maxSupply} {props.code}
+              </td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
           <tr className="table-row-crypto-specific grey-color-row">
             <th className="table-header-crypto-specific">All Time High</th>
-            <td className="table-data-crypto-specific">
-              $ {props.allTimeHigh} USD
-            </td>
+            {AllTimeHigh ? (
+              <td className="table-data-crypto-specific">
+                $ {props.allTimeHigh} USD
+              </td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
           <tr className="table-row-crypto-specific">
             <th className="table-header-crypto-specific">All Time Low</th>
-            <td className="table-data-crypto-specific">
-              $ {props.allTimeLow} USD
-            </td>
+            {AllTimeLow ? (
+              <td className="table-data-crypto-specific">
+                $ {props.allTimeLow} USD
+              </td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
           <tr className="table-row-crypto-specific grey-color-row">
             <th className="table-header-crypto-specific">52 Week High / Low</th>
-            <td className="table-data-crypto-specific">
-              {`$ ${props.a52weekHigh} USD / $ ${props.a52weekLow} USD`}
-            </td>
+            {a52WeekHigh && a52WeekLow ? (
+              <td className="table-data-crypto-specific">
+                {`$ ${props.a52weekHigh} USD / $ ${props.a52weekLow} USD`}
+              </td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
           <tr className="table-row-crypto-specific">
             <th className="table-header-crypto-specific">90 Day High / Low</th>
-            <td className="table-data-crypto-specific">
-              {`$ ${props.a90dayHigh} USD / $ ${props.a90dayLow} USD`}
-            </td>
+            {a90DayHigh && a90DayLow ? (
+              <td className="table-data-crypto-specific">
+                {`$ ${props.a90dayHigh} USD / $ ${props.a90dayLow} USD`}
+              </td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
           <tr className="table-row-crypto-specific grey-color-row">
             <th className="table-header-crypto-specific">30 Day High / Low</th>
-            <td className="table-data-crypto-specific">
-              {`$ ${props.a30dayHigh} USD / $ ${props.a30dayLow} USD`}
-            </td>
+            {a30DayHigh && a30DayLow ? (
+              <td className="table-data-crypto-specific">
+                {`$ ${props.a30dayHigh} USD / $ ${props.a30dayLow} USD`}
+              </td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
           <tr className="table-row-crypto-specific">
             <th className="table-header-crypto-specific">7 Day High / Low</th>
-            <td className="table-data-crypto-specific">
-              {`$ ${props.a7dayHigh} USD / $ ${props.a7dayLow} USD`}
-            </td>
+            {a7DayHigh && a7DayLow ? (
+              <td className="table-data-crypto-specific">
+                {`$ ${props.a7dayHigh} USD / $ ${props.a7dayLow} USD`}
+              </td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
           <tr className="table-row-crypto-specific grey-color-row">
             <th className="table-header-crypto-specific">24 Hour High / Low</th>
-            <td className="table-data-crypto-specific">
-              {`$ ${props.a24hourHigh} USD / $ ${props.a24hourLow} USD`}
-            </td>
+            {a24HourHigh && a24HourLow ? (
+              <td className="table-data-crypto-specific">
+                {`$ ${props.a24hourHigh} USD / $ ${props.a24hourLow} USD`}
+              </td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
           <tr className="table-row-crypto-specific">
             <th className="table-header-crypto-specific">
               Yesterday's High / Low{" "}
             </th>
-            <td className="table-data-crypto-specific">
-              {`$ ${props.yesterdaysHigh} USD / $ ${props.yesterdaysLow} USD`}
-            </td>
+            {YesterdaysHigh && YesterdaysLow ? (
+              <td className="table-data-crypto-specific">
+                {`$ ${props.yesterdaysHigh} USD / $ ${props.yesterdaysLow} USD`}
+              </td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
           <tr className="table-row-crypto-specific grey-color-row">
             <th className="table-header-crypto-specific">
               Yesterday's Open / Close{" "}
             </th>
-            <td className="table-data-crypto-specific">
-              {`$ ${props.yesterdaysOpen} USD / $ ${props.yesterdaysClose} USD`}
-            </td>
+            {YesterdaysOpen && YesterdaysClose ? (
+              <td className="table-data-crypto-specific">
+                {`$ ${props.yesterdaysOpen} USD / $ ${props.yesterdaysClose} USD`}
+              </td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
           <tr className="table-row-crypto-specific ">
             <th className="table-header-crypto-specific">
               Yesterday's Change{" "}
             </th>
-            <td className="table-data-crypto-specific">
-              $ {props.yesterdaysChange} USD
-            </td>
+            {YesterdaysChange ? (
+              <td className="table-data-crypto-specific">
+                $ {props.yesterdaysChange} USD
+              </td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
           <tr className="table-row-crypto-specific grey-color-row">
             <th className="table-header-crypto-specific">
               Yesterday's Volume{" "}
             </th>
-            <td className="table-data-crypto-specific">
-              $ {props.yesterdaysVolume} USD
-            </td>
+            {YesterdaysVolume ? (
+              <td className="table-data-crypto-specific">
+                $ {props.yesterdaysVolume} USD
+              </td>
+            ) : (
+              <td className="table-data-crypto-specific">No Data</td>
+            )}
           </tr>
         </table>
       </div>
