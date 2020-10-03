743da7e9fe79e8cdc0419078f56ba04870f7e61c
XX989tmx <issacgravity8@gmail.com>
2020-10-04 06:40:46 +0900

    adds video selector data

---
+++ src/videos/data/video-date-sort-data.js
@@ -0,0 +1,8 @@
+let dateOptions = [
+  "today",
+  "this week",
+  "this month",
+  "within three month",
+  "this year",
+  "previous year",
+];
---
+++ src/videos/data/video-general-sort-data.js
@@ -0,0 +1,12 @@
+let sortOptions = [
+  "Trending",
+  "Random",
+  "Highest Rated",
+  "Least Rated",
+  "Most Commented",
+  "Least Commented",
+  "Most Viewed",
+  "Least Viewed",
+  "4K",
+  "HD"
+];
