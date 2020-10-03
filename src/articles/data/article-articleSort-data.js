cd9e20ffdd5b88e7e8ddc22ca97fe6ac1298d8db
XX989tmx <issacgravity8@gmail.com>
2020-10-04 06:27:46 +0900

    adds article-articleSort-data.js

---
+++ src/articles/data/article-articleSort-data.js
@@ -0,0 +1,10 @@
+let sortOptions = [
+  "HighestToCheapest",
+  "CheapestToHighest",
+  "FromOldest",
+  "FromLatest",
+  "Most viewed",
+  "Least viewed",
+  "Highest Favorite Count",
+  "Lowest Favorite Count",
+];
