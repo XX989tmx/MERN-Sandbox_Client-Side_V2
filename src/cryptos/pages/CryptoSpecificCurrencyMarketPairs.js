ba5636e1398c150af48ca02cf78ef64987062cc2
XX989tmx <issacgravity8@gmail.com>
2020-10-05 09:57:23 +0900

    finish to add a basic structure to CryptoSpecificCurrencyMarketPairs related pages and its components

--- src/cryptos/pages/CryptoSpecificCurrencyMarketPairs.js
+++ src/cryptos/pages/CryptoSpecificCurrencyMarketPairs.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import CryptoSpecificCurrencyMarketPairsList from '../components/CryptoSpecificCurrencyMarketPairsList';
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
+import CryptoSpecificCurrencyMarketPairsList from "../components/CryptoSpecificCurrencyMarketPairsList";
+import { useParams, useHistory, Link } from "react-router-dom";
+import "./CryptoSpecificCurrencyMarketPairs.css";
 
 const CryptoSpecificCurrencyMarketPairs = () => {
-    return (
-        <div>
-            <CryptoSpecificCurrencyMarketPairsList />
-        </div>
-    );
-}
+  const queryName = useParams().queryName;
+  const [CryptoData, setCryptoData] = useState([]);
+  useEffect(() => {
+    const fetch = (params) => {
+      Axios.get(
+        process.env.REACT_APP_BACKEND_URL +
+          `/cryptos/currencies/${queryName}/market_pairs`
+      )
+        .then((response) => {
+          console.log(response.data.crypto);
+          setCryptoData(response.data.crypto);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    fetch();
+  }, []);
+  return (
+    <div className="crypto-specific-market-pairs-container">
+      <CryptoSpecificCurrencyMarketPairsList CryptoData={CryptoData} />
+      <div className="related-cryptocurrencies"></div>
+    </div>
+  );
+};
 
 export default CryptoSpecificCurrencyMarketPairs;
