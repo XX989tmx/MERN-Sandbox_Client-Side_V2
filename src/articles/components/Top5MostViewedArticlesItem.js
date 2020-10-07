ee7c58b695aaeb63763c5f7159c2c15227f0a1a3
XX989tmx <issacgravity8@gmail.com>
2020-10-08 04:19:52 +0900

    adds Link to 'top 5' item.js

--- src/articles/components/Top5MostViewedArticlesItem.js
+++ src/articles/components/Top5MostViewedArticlesItem.js
@@ -1,16 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Top5MostViewedArticlesItem = (props) => {
   return (
-    <li>
-      <div>
-        <span> Rank {props.index + 1}</span>
-        {/* <img src={props.images[0]}/> */}
-        {props.title}
-        {props.author.name}
-        {props.price}
-      </div>
-    </li>
+    <Link to={`/get_specific_article_by_id/${props.id}`}>
+      <li>
+        <div>
+          <span> Rank {props.index + 1}</span>
+          {/* <img src={props.images[0]}/> */}
+          {props.title}
+          {props.author.name}
+          {props.price}
+        </div>
+      </li>
+    </Link>
   );
 };
 
