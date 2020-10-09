14c687154b644c0a79d8c89b7622f210beb77ad6
XX989tmx <issacgravity8@gmail.com>
2020-10-10 05:42:24 +0900

    adds link to s to f child components

--- src/crypto-converters/components/AttractiveItem.js
+++ src/crypto-converters/components/AttractiveItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const AttractiveItem = (props) => {
     return (
+      <Link to={`/cryptos/currencies/${props.queryName}`}>
         <li>
-            {props.name}
-            {props.code}
+          {props.name}
+          {props.code}
         </li>
+      </Link>
     );
 }
 
