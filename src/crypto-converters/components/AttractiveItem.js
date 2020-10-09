83bb139dcde807c061ad75973dbd145dd3f824c5
XX989tmx <issacgravity8@gmail.com>
2020-10-10 05:12:18 +0900

    passed attractive array s props to child component

--- src/crypto-converters/components/AttractiveItem.js
+++ src/crypto-converters/components/AttractiveItem.js
@@ -3,7 +3,8 @@ import React from 'react';
 const AttractiveItem = (props) => {
     return (
         <li>
-            
+            {props.name}
+            {props.code}
         </li>
     );
 }
