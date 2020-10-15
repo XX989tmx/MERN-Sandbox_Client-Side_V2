26da0374bd30e5b7d1999d7d0bdf1c6909a3e627
XX989tmx <issacgravity8@gmail.com>
2020-10-16 07:54:11 +0900

    adds map rendering to following

--- src/users/components/GetSpecificUserItem.js
+++ src/users/components/GetSpecificUserItem.js
@@ -8,12 +8,21 @@ const GetSpecificUserItem = (props) => {
       {/* {props.articles} */}
       {props.email}
       {/* {props.followedBy} */}
-      {/* {props.following} */}
+      <div>
+        <h4>People This User Is Following(Following)</h4>
+        {props.following.map((v, i) => {
+          return (
+            <a href={`/getSpecificUser/${v.id}`}>
+              <h5 key={i}>{v.name}</h5>
+            </a>
+          );
+        })}
+      </div>
       {props.name}
       {props.image}
       {/* {props.profile} */}
       <div>
-      <h4>Articles This User Stared</h4>
+        <h4>Articles This User Stared</h4>
         {props.staredArticles.map((v, i) => {
           return (
             <Link to={`/get_specific_article_by_id/${v.id}`}>
