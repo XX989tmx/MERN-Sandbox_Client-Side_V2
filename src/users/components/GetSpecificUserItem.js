3061822a53db3cfa53c7b07aa63b5ed1da3d3266
XX989tmx <issacgravity8@gmail.com>
2020-10-16 08:09:42 +0900

    adds map rendering to articles

--- src/users/components/GetSpecificUserItem.js
+++ src/users/components/GetSpecificUserItem.js
@@ -5,8 +5,26 @@ const GetSpecificUserItem = (props) => {
   return (
     <li>
       {props.id}
-      {/* {props.articles} */}
+      <h3>{props.name}</h3>
+      <img
+        src={props.image}
+        alt=""
+        style={{ width: "100px", height: "100px" }}
+      />
       {props.email}
+
+      <div>
+        <h4>{props.name}'s Articles</h4>
+        {props.articles.map((v, i) => {
+          return (
+            <Link to={`/get_specific_article_by_id/${v.id}`}>
+              {" "}
+              <h5 key={i}>{v.title}</h5>
+            </Link>
+          );
+        })}
+      </div>
+
       <div>
         <h4>People Following This User(Followers)</h4>
         {props.followedBy.map((v, i) => {
@@ -27,15 +45,14 @@ const GetSpecificUserItem = (props) => {
           );
         })}
       </div>
-      {props.name}
-      {props.image}
+
       {/* {props.profile} */}
       <div>
         <h4>Articles This User Stared</h4>
         {props.staredArticles.map((v, i) => {
           return (
             <Link to={`/get_specific_article_by_id/${v.id}`}>
-              <h5>{v.title}</h5>
+              <h5 key={i}>{v.title}</h5>
             </Link>
           );
         })}
