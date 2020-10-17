5c8d59cd757f3fa05df29b3e733dcec73cc5790d
XX989tmx <issacgravity8@gmail.com>
2020-10-18 08:48:38 +0900

    adds link to comments. destination is user specific page.

--- src/articles/pages/GetSpecificArticleById.js
+++ src/articles/pages/GetSpecificArticleById.js
@@ -186,23 +186,35 @@ const GetSpecificArticleById = () => {
                   {Comments.map((v, i) => {
                     return (
                       <div key={i}>
-                        <span>
-                          <img
-                            style={{ width: "25px", height: "25px" }}
-                            src={v.user.image}
-                            alt=""
-                          />
-                        </span>
-                        <span style={{ paddingLeft: "10px", fontSize: "16px" }}>
-                          {v.user.name}
-                        </span>
-                        <span style={{ paddingLeft: "10px", fontSize: "16px" }}>
-                          {v.comment}
-                        </span>
-                        <span style={{ paddingLeft: "10px", fontSize: "13px" }}>
-                          {new Date(v.createdAt).toDateString()}
-                        </span>
-                      </div>
+                        <Link to={`/getSpecificUser/${v.user.id}`}>
+                          {" "}
+                          <div>
+                            <span>
+                              <img
+                                style={{ width: "25px", height: "25px" }}
+                                src={v.user.image}
+                                alt=""
+                              />
+                            </span>
+                            <span
+                              style={{ paddingLeft: "10px", fontSize: "16px" }}
+                            >
+                              {v.user.name}
+                            </span>
+                            <span
+                              style={{ paddingLeft: "10px", fontSize: "16px" }}
+                            >
+                              {v.comment}
+                            </span>
+                            <span
+                              style={{ paddingLeft: "10px", fontSize: "13px" }}
+                            >
+                              {new Date(v.createdAt).toDateString()}
+                            </span>
+                          </div>
+                          </Link>
+                        </div>
+                      
                     );
                   })}
                 </div>
