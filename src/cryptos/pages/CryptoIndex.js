f3c4e108336109bd8c100cd2c8f743462dff2d9d
XX989tmx <issacgravity8@gmail.com>
2020-10-19 23:46:27 +0900

    adds externalLink to cryptoIndex

--- src/cryptos/pages/CryptoIndex.js
+++ src/cryptos/pages/CryptoIndex.js
@@ -1,6 +1,7 @@
 import Axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import FooterMainNavigation from "../../shared/components/Footer/FooterMainNavigation";
+import ExternalLink from "../../shared/components/UIElements/ExternalLink";
 import MoveToTopButton from "../../shared/components/UIElements/MoveToTopButton";
 import CryptoIndexList from "../components/CryptoIndexList";
 import CryptoIndexTableHeader from "../components/CryptoIndexTableHeader";
@@ -26,6 +27,11 @@ const CryptoIndex = () => {
       <div className="crypto-index-container">
         <div>
           <h2>Top 100 Cryptocurrencies by Market Capitalization</h2>
+          <ExternalLink
+            to="https://coinmarketcap.com/"
+            className="natural"
+            text="Data Provided by CoinMarketCap"
+          />
           {/* <table className="thead-tag">
         <thead className="thead-crypto-index"> */}
           <CryptoIndexTableHeader />
