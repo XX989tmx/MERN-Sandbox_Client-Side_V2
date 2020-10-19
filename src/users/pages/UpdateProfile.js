7bdb8acfb3f57f423c65f1c15f648bfb11a0f51a
XX989tmx <issacgravity8@gmail.com>
2020-10-19 09:19:02 +0900

    adds dependency to updateProfile.js

--- src/users/pages/UpdateProfile.js
+++ src/users/pages/UpdateProfile.js
@@ -1,5 +1,15 @@
-import React from 'react';
-
+import Axios from 'axios';
+import React, { useContext } from 'react';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Button from '../../shared/components/FormElements/Button';
+import Input from "../../shared/components/FormElements/Input";
+import { AuthContext } from "../../shared/context/auth-context";
+import { useForm } from "../../shared/hooks/form-hook";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
 const UpdateProfile = () => {
     return (
         <div>
