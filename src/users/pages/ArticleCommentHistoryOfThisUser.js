0f8c6bddbbc117cd733ccada6fef1baf7eb64bb2
XX989tmx <issacgravity8@gmail.com>
2020-10-19 23:33:50 +0900

    adds class and style to articleCommentHistoryOfThisUser.js

--- src/users/pages/ArticleCommentHistoryOfThisUser.js
+++ src/users/pages/ArticleCommentHistoryOfThisUser.js
@@ -3,33 +3,39 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useParams, useHistory } from "react-router-dom";
+import FooterMainNavigation from "../../shared/components/Footer/FooterMainNavigation";
+import MoveToTopButton from "../../shared/components/UIElements/MoveToTopButton";
 import ArticleCommentHistoryOfThisUserList from "../components/ArticleCommentHistoryOfThisUserList";
+import "./ArticleCommentHistoryOfThisUser.css";
 
 const ArticleCommentHistoryOfThisUser = () => {
-const userId = useParams().userId;
-const [UserArray, setUserArray] = useState([]);
-useEffect(() => {
-  const fetch = async (params) => {
-    try {
-      const response = await Axios.get(
-        process.env.REACT_APP_BACKEND_URL + `/users/getSpecificUser/${userId}`
-      );
-      const data = response.data;
-      const user = data.result;
-      console.log(user);
-      setUserArray(user);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  fetch();
-}, []);
-
+  const userId = useParams().userId;
+  const [UserArray, setUserArray] = useState([]);
+  useEffect(() => {
+    const fetch = async (params) => {
+      try {
+        const response = await Axios.get(
+          process.env.REACT_APP_BACKEND_URL + `/users/getSpecificUser/${userId}`
+        );
+        const data = response.data;
+        const user = data.result;
+        console.log(user);
+        setUserArray(user);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetch();
+  }, []);
 
   return (
-    <div>
-      <ArticleCommentHistoryOfThisUserList UserArray={UserArray} />
-    </div>
+    <React.Fragment>
+      <div className="article-comment-history-of-this-user-container">
+        <ArticleCommentHistoryOfThisUserList UserArray={UserArray} />
+      </div>
+      <MoveToTopButton />
+      <FooterMainNavigation />
+    </React.Fragment>
   );
 };
 
