b64f1066a1370594ea6b1aa1ab6dfa17834be089
XX989tmx <issacgravity8@gmail.com>
2020-10-24 20:55:56 +0900

    adds shared sidebar and minimum layout to billing page

--- src/users/pages/Billing.js
+++ src/users/pages/Billing.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React from "react";
+import MyProfileSideNavigation from "../components/MyProfileSideNavigation";
+import MoveToTopButton from "../../shared/components/UIElements/MoveToTopButton";
+import FooterMainNavigation from "../../shared/components/Footer/FooterMainNavigation";
 
 const Billing = () => {
-    return <div>Billing</div>;
-}
+  return (
+    <React.Fragment>
+      {" "}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          paddingLeft: "20%",
+        }}
+      >
+        <div
+          style={{ width: "1080px", minHeight: "100vh" }}
+          className="address-container"
+        >
+          <div>
+            <h1>Billing</h1>
+            <h4>registered payment method</h4>
+            <h4>add payment method</h4>
+            <h4>update payment method</h4>
+            <h4>delete payment method</h4>
+            <h4>check invoice</h4>
+            <h4>invoice this month</h4>
+            <h4>invoice this year</h4>
+            <h4>invoice all</h4>
+          </div>
+        </div>
+        <div
+          style={{ padding: "20px", width: "400px" }}
+          className="my-profile-sidebar-area"
+        >
+          <MyProfileSideNavigation />
+        </div>
+      </div>
+      <MoveToTopButton />
+      <FooterMainNavigation />
+    </React.Fragment>
+  );
+};
 
 export default Billing;
