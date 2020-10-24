97895ec9c7ebf75b71741df3fc66ec1c607a13d7
XX989tmx <issacgravity8@gmail.com>
2020-10-24 20:47:55 +0900

    adds shared sidebar and minimum layout to address page

--- src/users/pages/Address.js
+++ src/users/pages/Address.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React from "react";
+import MoveToTopButton from "../../shared/components/UIElements/MoveToTopButton";
+import FooterMainNavigation from "../../shared/components/Footer/FooterMainNavigation";
+import MyProfileSideNavigation from "../components/MyProfileSideNavigation";
 
 const Address = () => {
-    return <div>Address</div>;
-}
+  return (
+    <React.Fragment>
+      {" "}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          paddingLeft: "20%",
+        }}
+      >
+        <div
+          style={{ width: "1080px", minHeight: "100vh" }}
+          className="address-container"
+        >
+          <div>
+            {" "}
+            <h1> Address</h1>
+            <h4>default address</h4>
+            <h4>check all address</h4>
+            <h4>add address</h4>
+            <h4>update address</h4>
+            <h4>delete address</h4>
+          </div>
+        </div>
+        <div
+          style={{ padding: "20px", width: "400px" }}
+          className="my-profile-sidebar-area"
+        >
+          <MyProfileSideNavigation />
+        </div>
+      </div>
+      <MoveToTopButton />
+      <FooterMainNavigation />
+    </React.Fragment>
+  );
+};
 
 export default Address;
