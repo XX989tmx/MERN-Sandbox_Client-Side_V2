0700e81bcad4f83f733c7e5a4368d454c508701e
XX989tmx <issacgravity8@gmail.com>
2020-10-24 20:59:05 +0900

    adds shared sidebar and minimum layout to followers page

--- src/users/pages/Followers.js
+++ src/users/pages/Followers.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React from "react";
+import MyProfileSideNavigation from "../components/MyProfileSideNavigation";
+import MoveToTopButton from "../../shared/components/UIElements/MoveToTopButton";
+import FooterMainNavigation from "../../shared/components/Footer/FooterMainNavigation";
 
 const Followers = () => {
-    return <div>Followers</div>;
-}
+  return (
+    <React.Fragment>
+      {" "}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          paddingLeft: "20%",
+        }}
+      >
+        <div
+          style={{ width: "1080px", minHeight: "100vh" }}
+          className="address-container"
+        >
+          <div>
+            {" "}
+            <h1> Followers</h1>
+          </div>
+        </div>
+        <div
+          style={{ padding: "20px", width: "400px" }}
+          className="my-profile-sidebar-area"
+        >
+          <MyProfileSideNavigation />
+        </div>
+      </div>
+      <MoveToTopButton />
+      <FooterMainNavigation />
+    </React.Fragment>
+  );
+};
 
 export default Followers;
