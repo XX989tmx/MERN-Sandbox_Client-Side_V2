d5b27b0ad4909a5d30e2bdbc01fbf68ef46df32d
XX989tmx <issacgravity8@gmail.com>
2020-10-24 20:51:22 +0900

    adds shared sidebar and minimum layout to analytics page

--- src/users/pages/Analytics.js
+++ src/users/pages/Analytics.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React from "react";
+import MyProfileSideNavigation from "../components/MyProfileSideNavigation";
+import FooterMainNavigation from "../../shared/components/Footer/FooterMainNavigation";
+import MoveToTopButton from "../../shared/components/UIElements/MoveToTopButton";
 
 const Analytics = () => {
-    return <div>Analytics</div>;
-}
+  return (
+    <React.Fragment>
+      {" "}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          paddingLeft: "20%",
+        }}
+      >
+        <div
+          style={{ width: "1080px", minHeight: "100vh" }}
+          className="address-container"
+        >
+          <div>
+            <h1>Analytics</h1>
+          </div>
+        </div>
+        <div
+          style={{ padding: "20px", width: "400px" }}
+          className="my-profile-sidebar-area"
+        >
+          <MyProfileSideNavigation />
+        </div>
+      </div>
+      <MoveToTopButton />
+      <FooterMainNavigation />
+    </React.Fragment>
+  );
+};
 
 export default Analytics;
