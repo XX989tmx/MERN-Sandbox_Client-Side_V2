a685854eac5eddf7d14c107eba56d8f2ca8732cb
XX989tmx <issacgravity8@gmail.com>
2020-10-27 07:54:29 +0900

    adds view count props and staredBy props to stared articles item js

--- src/users/components/StaredArticlesItem.js
+++ src/users/components/StaredArticlesItem.js
@@ -395,6 +395,10 @@ const StaredArticlesItem = (props) => {
                     </span>
                   )}
                 </div>
+                <div>
+                  <span>{props.viewCount} viewed</span>
+                  <span>{props.staredBy.length} star</span>
+                </div>
                 <div className="date-created-box">
                   <p className="date-created-tag">
                     Date Created: {new Date(props.date_created).toDateString()}
