528a9a832579f9de754d5908e6cdcd4bb8de528a
XX989tmx <issacgravity8@gmail.com>
2020-10-31 08:18:47 +0900

    adds modal

--- src/users/components/AddressItem.js
+++ src/users/components/AddressItem.js
@@ -1,62 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import Button from "../../shared/components/FormElements/Button";
 import { AuthContext } from "../../shared/context/auth-context";
+import Modal from "../../shared/components/UIElements/Modal";
 
 const AddressItem = (props) => {
   const auth = useContext(AuthContext);
+  const [ShowDeleteAddressModal, setShowDeleteAddressModal] = useState(false);
+
+  const openDeleteAddressModalHandler = (params) => {
+    setShowDeleteAddressModal(true);
+  };
+
+  const closeDeleteAddressModalHandler = (params) => {
+    setShowDeleteAddressModal(false);
+  };
   return (
-    <li>
-      <div
-        style={{
-          width: "310px",
-          height: "300px",
-          border: "solid 1px grey",
-          borderRadius: "10px",
-        }}
+    <React.Fragment>
+      <Modal
+        show={ShowDeleteAddressModal}
+        onCancel={closeDeleteAddressModalHandler}
+        header="Delete Address?"
+        footer={
+          <React.Fragment>
+            <Button btnBlack onClick={closeDeleteAddressModalHandler}>
+              Cancel
+            </Button>
+            <Button danger>Delete</Button>
+          </React.Fragment>
+        }
       >
-        <div style={{ height: "10%" }}></div>
-        <div style={{ height: "80%", borderTop: "1px solid grey" }}>
-          <div
-            style={{
-              paddingLeft: "20px",
-              paddingTop: "10px",
-              fontSize: "15px",
-              fontWeight: "500",
-              lineHeight: "20px",
-            }}
-          >
-            {" "}
-            <span>{props.name}</span>
-            <br />
-            <span>{props.zip_code}</span>
-            <br />
-            <span>{props.todoufuken}</span>
-            <br />
-            <span>{props.address_info1}</span>
-            <br />
-            <span>{props.address_info2}</span>
-            <br />
-            <span>{props.country}</span>
-            <br />
-            <span>Phone: </span> <span>{props.phone_number}</span>
-            <br />
-            <span>{props.email}</span>
-            <br />
-            <span>{props.company}</span>
-            <br />
+        <p style={{ fontSize: "15px", fontWeight: "500", lineHeight: "20px" }}>
+          Are you sure you want to delete this address?
+        </p>
+      </Modal>
+      <li>
+        <div
+          style={{
+            width: "310px",
+            height: "300px",
+            border: "solid 1px grey",
+            borderRadius: "10px",
+          }}
+        >
+          <div style={{ height: "10%" }}></div>
+          <div style={{ height: "80%", borderTop: "1px solid grey" }}>
+            <div
+              style={{
+                paddingLeft: "20px",
+                paddingTop: "10px",
+                fontSize: "15px",
+                fontWeight: "500",
+                lineHeight: "20px",
+              }}
+            >
+              {" "}
+              <span>{props.name}</span>
+              <br />
+              <span>{props.zip_code}</span>
+              <br />
+              <span>{props.todoufuken}</span>
+              <br />
+              <span>{props.address_info1}</span>
+              <br />
+              <span>{props.address_info2}</span>
+              <br />
+              <span>{props.country}</span>
+              <br />
+              <span>Phone: </span> <span>{props.phone_number}</span>
+              <br />
+              <span>{props.email}</span>
+              <br />
+              <span>{props.company}</span>
+              <br />
+            </div>
+          </div>
+          <div style={{ height: "10%" }}>
+            <Link to={`/${auth.userId}/updateAddress/${props.id}`}>
+              <Button>Update</Button>
+            </Link>
+
+            <Button onClick={openDeleteAddressModalHandler}>Delete</Button>
           </div>
         </div>
-        <div style={{ height: "10%" }}>
-          <Link to={`/${auth.userId}/updateAddress/${props.id}`}>
-            <Button>Update</Button>
-          </Link>
-
-          <Button>Delete</Button>
-        </div>
-      </div>
-    </li>
+      </li>
+    </React.Fragment>
   );
 };
 
