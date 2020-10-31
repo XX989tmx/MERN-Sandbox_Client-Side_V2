b67ff7a240db1fe1ad7f1bef2a9b756ff7111bf7
XX989tmx <issacgravity8@gmail.com>
2020-11-01 06:19:55 +0900

    adds my loading spinner to category index js

--- src/articles/pages/CategoryIndex.js
+++ src/articles/pages/CategoryIndex.js
@@ -1,18 +1,21 @@
 import React, { useContext, useState, useEffect } from "react";
 import { useHttpClient } from "../../shared/hooks/http-hook";
 import CategoryIndexList from "../components/CategoryIndexList";
+import MyLoadingSpinner from "../../shared/components/UIElements/MyLoadingSpinner";
 
 import "./CategoryIndex.css";
 import FooterMainNavigation from "../../shared/components/Footer/FooterMainNavigation";
 import { Link } from "react-router-dom";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { findAllByDisplayValue } from "@testing-library/react";
 
 const CategoryIndex = () => {
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [CategoryIndexData, setCategoryIndexData] = useState([]);
-
+  const [IsLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const getCategoryIndex = async (params) => {
+      setIsLoading(true);
       try {
         const responseData = await sendRequest(
           process.env.REACT_APP_BACKEND_URL + `/articles/categoryIndex`
@@ -23,33 +26,35 @@ const CategoryIndex = () => {
         // console.log(responseData.responseArray[0].categoryName);
         // console.log(responseData.responseArray[0].count);
       } catch (error) {}
+      setIsLoading(false);
     };
     getCategoryIndex();
   }, [sendRequest]);
 
   return (
     <React.Fragment>
-      {isLoading && (
-        <div className="center">
-          <LoadingSpinner />
+      {IsLoading && <MyLoadingSpinner />}
+
+      {!IsLoading && CategoryIndexData && (
+        <div>
+          {" "}
+          <div className="category-index-container">
+            <div className="main-container">
+              <div>
+                <CategoryIndexList CategoryIndexData={CategoryIndexData} />
+              </div>
+              <div>
+                <span>
+                  <Link to={`/articles`}>Article Index</Link>
+                </span>
+              </div>
+            </div>
+
+            {/* <div className="side-container"></div> */}
+          </div>
+          <FooterMainNavigation />
         </div>
       )}
-      <div className="category-index-container">
-        {!isLoading && CategoryIndexData && (
-          <div className="main-container">
-            <div>
-              <CategoryIndexList CategoryIndexData={CategoryIndexData} />
-            </div>
-            <div>
-              <span>
-                <Link to={`/articles`}>Article Index</Link>
-              </span>
-            </div>
-          </div>
-        )}
-        {/* <div className="side-container"></div> */}
-      </div>
-      <FooterMainNavigation />
     </React.Fragment>
   );
 };
