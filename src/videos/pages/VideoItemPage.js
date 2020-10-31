fce8b2c810ef3975db2702aa1e5873a00884b405
XX989tmx <issacgravity8@gmail.com>
2020-11-01 07:34:48 +0900

    adds my loading spinner to video item page js

--- src/videos/pages/VideoItemPage.js
+++ src/videos/pages/VideoItemPage.js
@@ -8,6 +8,7 @@ import VideoItems from "../components/VideoItems";
 import VideoByIdItem from "../components/VideoByIdItem";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import MoveToTopButton from "../../shared/components/UIElements/MoveToTopButton";
+import MyLoadingSpinner from "../../shared/components/UIElements/MyLoadingSpinner";
 
 import "./VideoItemPage.css";
 import FooterMainNavigation from "../../shared/components/Footer/FooterMainNavigation";
@@ -23,8 +24,10 @@ const VideoItemPage = () => {
   const [videoPersonsArray, setvideoPersonsArray] = useState([]);
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [RelatedVideos, setRelatedVideos] = useState([]);
+  const [IsLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const getVideoById = async (params) => {
+      setIsLoading(true);
       try {
         const responseData = await sendRequest(
           process.env.REACT_APP_BACKEND_URL + `/videos/${videoId}`
@@ -39,64 +42,73 @@ const VideoItemPage = () => {
         console.log(responseData.video.comments);
         console.log(responseData.video.title);
       } catch (error) {}
+
       // window.scrollTo(0, 0);
       try {
-        const response = await Axios.get(process.env.REACT_APP_BACKEND_URL + `/videos/get_related_videos/${videoId}`);
+        const response = await Axios.get(
+          process.env.REACT_APP_BACKEND_URL +
+            `/videos/get_related_videos/${videoId}`
+        );
         const data = response.data;
         console.log(data);
         setRelatedVideos(data.relatedVideosArrayCapped20);
       } catch (error) {
         console.log(error);
       }
+      function moveToTop(params) {
+        window.scrollTo(0, 0);
+      }
+      moveToTop();
+      setIsLoading(false);
     };
     getVideoById();
   }, [sendRequest]);
 
   return (
     <React.Fragment>
-      {isLoading && (
-        <div className="center">
-          <LoadingSpinner />
-        </div>
-      )}
-      {!isLoading && VideoById && (
-        <div className="video-item-page-container">
-          <div className="video-item-main-section">
-            {/* <p>
+      {IsLoading && <MyLoadingSpinner />}
+      {!IsLoading &&
+        VideoById &&
+        RelatedVideos && (
+          <div>
+            <div className="video-item-page-container">
+              <div className="video-item-main-section">
+                {/* <p>
               個別のビデオ表示のためには2通りの可能性。1videoItem child
               componentに渡して表示、2個別のビデオ表示用のchild
               componentを作り、それに渡す（youtubeの個別表示ページのようなレイアウト）
             </p> */}
-            <VideoByIdItem
-              title={VideoById.title}
-              description={VideoById.description}
-              src={VideoById.src}
-              videoTagsArray={videoTagsArray}
-              videoCategoriesArray={videoCategoriesArray}
-              videoPersonsArray={videoPersonsArray}
-              id={VideoById.id}
-              date_created={VideoById.date_created}
-              views={VideoById.views}
-              liked={VideoById.liked}
-              disliked={VideoById.disliked}
-              comments={VideoCommentsArray}
-            />
+                <VideoByIdItem
+                  title={VideoById.title}
+                  description={VideoById.description}
+                  src={VideoById.src}
+                  videoTagsArray={videoTagsArray}
+                  videoCategoriesArray={videoCategoriesArray}
+                  videoPersonsArray={videoPersonsArray}
+                  id={VideoById.id}
+                  date_created={VideoById.date_created}
+                  views={VideoById.views}
+                  liked={VideoById.liked}
+                  disliked={VideoById.disliked}
+                  comments={VideoCommentsArray}
+                />
 
-            {/* <VideoList items={VideoById} /> */}
-            {/* each video item page. place video player box component & video link item
+                {/* <VideoList items={VideoById} /> */}
+                {/* each video item page. place video player box component & video link item
       List&video link item. */}
-            {/* <VideoPlayerBox></VideoPlayerBox>
+                {/* <VideoPlayerBox></VideoPlayerBox>
         
             <VideoLinkItemList></VideoLinkItemList> */}
+              </div>
+              <div className="video-item-side-bar-section">
+                <h4>Related Videos</h4>
+                <RelatedVideosList RelatedVideos={RelatedVideos} />
+              </div>
+              <MoveToTopButton />
+            </div>
+            <FooterMainNavigation />
           </div>
-          <div className="video-item-side-bar-section">
-            <h4>Related Videos</h4>
-            <RelatedVideosList RelatedVideos={RelatedVideos} />
-          </div>
-          <MoveToTopButton />
-        </div>
-      )}
-      {!isLoading && VideoById && <FooterMainNavigation />}
+        )}
     </React.Fragment>
   );
 };
