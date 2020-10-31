bbc5342762f946c05a9e239c1b2a3c591ceb9f67
XX989tmx <issacgravity8@gmail.com>
2020-11-01 08:53:24 +0900

    adds my loading spinner to crypto index

--- src/cryptos/pages/CryptoIndex.js
+++ src/cryptos/pages/CryptoIndex.js
@@ -5,17 +5,25 @@ import ExternalLink from "../../shared/components/UIElements/ExternalLink";
 import MoveToTopButton from "../../shared/components/UIElements/MoveToTopButton";
 import CryptoIndexList from "../components/CryptoIndexList";
 import CryptoIndexTableHeader from "../components/CryptoIndexTableHeader";
+import MyLoadingSpinner from "../../shared/components/UIElements/MyLoadingSpinner";
 import "./CryptoIndex.css";
 const CryptoIndex = () => {
   const [CryptoArray, setCryptoArray] = useState([]);
+  const [IsLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     const fetch = (params) => {
       Axios.get(process.env.REACT_APP_BACKEND_URL + `/cryptos/index`)
         .then((response) => {
           console.log(response.data);
           console.log(response.data.cryptos);
           setCryptoArray(response.data.cryptos);
+          function moveToTop(params) {
+            window.scrollTo(0, 0);
+          }
+          moveToTop();
+          setIsLoading(false);
         })
         .catch((err) => {});
     };
@@ -24,25 +32,31 @@ const CryptoIndex = () => {
 
   return (
     <React.Fragment>
-      <div className="crypto-index-container">
+      {IsLoading && <MyLoadingSpinner />}
+      {!IsLoading && CryptoArray && (
         <div>
-          <h2>Top 100 Cryptocurrencies by Market Capitalization</h2>
-          <ExternalLink
-            to="https://coinmarketcap.com/"
-            className="natural"
-            text="Data Provided by CoinMarketCap"
-          />
-          {/* <table className="thead-tag">
+          {" "}
+          <div className="crypto-index-container">
+            <div>
+              <h2>Top 100 Cryptocurrencies by Market Capitalization</h2>
+              <ExternalLink
+                to="https://coinmarketcap.com/"
+                className="natural"
+                text="Data Provided by CoinMarketCap"
+              />
+              {/* <table className="thead-tag">
         <thead className="thead-crypto-index"> */}
-          <CryptoIndexTableHeader />
-          {/* </thead> */}
+              <CryptoIndexTableHeader />
+              {/* </thead> */}
 
-          <CryptoIndexList CryptoArray={CryptoArray} />
-          {/* </table> */}
+              <CryptoIndexList CryptoArray={CryptoArray} />
+              {/* </table> */}
+            </div>
+          </div>
+          <MoveToTopButton />
+          <FooterMainNavigation />
         </div>
-      </div>
-      <MoveToTopButton />
-      <FooterMainNavigation />
+      )}
     </React.Fragment>
   );
 };
