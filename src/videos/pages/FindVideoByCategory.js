8160e00e9bbf7b6836f16e7d0fa82cd38db1578a
XX989tmx <issacgravity8@gmail.com>
2020-11-01 07:41:02 +0900

    adds my loading spinner to find video by category js

--- src/videos/pages/FindVideoByCategory.js
+++ src/videos/pages/FindVideoByCategory.js
@@ -4,53 +4,61 @@ import { useHttpClient } from "../../shared/hooks/http-hook";
 import VideoList from "../components/VideoList";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import MoveToTopButton from "../../shared/components/UIElements/MoveToTopButton";
+import MyLoadingSpinner from "../../shared/components/UIElements/MyLoadingSpinner";
 
-import './FindVideoByCategory.css';
+import "./FindVideoByCategory.css";
 
 const FindVideoByCategory = () => {
-    const categories = useParams().categories;
-    const { isLoading, error, sendRequest, clearError } = useHttpClient();
-    const [CategorySortedVideos, setCategorySortedVideos] = useState([]);
-    const [CategoryVideoCount, setCategoryVideoCount] = useState();
+  const categories = useParams().categories;
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [CategorySortedVideos, setCategorySortedVideos] = useState([]);
+  const [CategoryVideoCount, setCategoryVideoCount] = useState();
 
-    useEffect(() => {
-      const getVideoByCategories = async (params) => {
-        try {
-          const responseData = await sendRequest(
-            process.env
-              .REACT_APP_BACKEND_URL + `/videos/get_video_by_categories/${categories}`
-          );
-          console.log(responseData);
-          setCategorySortedVideos(responseData.categoryMatchedVideos);
-          console.log(responseData.categoryMatchedVideos);
-          setCategoryVideoCount(responseData.countByCategory);
-          console.log(responseData.countByCategory);
-        } catch (error) {}
-        // window.scrollTo(0, 0);
-      };
-      getVideoByCategories();
-    }, [sendRequest]);
+  const [IsLoading, setIsLoading] = useState(false);
 
-    return (
-      <React.Fragment>
-        {isLoading && (
-          <div className="center">
-            <LoadingSpinner />
-          </div>
-        )}
-        {!isLoading && CategorySortedVideos && (
+  useEffect(() => {
+    const getVideoByCategories = async (params) => {
+      setIsLoading(true);
+      try {
+        const responseData = await sendRequest(
+          process.env.REACT_APP_BACKEND_URL +
+            `/videos/get_video_by_categories/${categories}`
+        );
+        console.log(responseData);
+        setCategorySortedVideos(responseData.categoryMatchedVideos);
+        console.log(responseData.categoryMatchedVideos);
+        setCategoryVideoCount(responseData.countByCategory);
+        console.log(responseData.countByCategory);
+      } catch (error) {}
+      function moveToTop(params) {
+        window.scrollTo(0, 0);
+      }
+      moveToTop();
+      setIsLoading(false);
+      // window.scrollTo(0, 0);
+    };
+    getVideoByCategories();
+  }, [sendRequest]);
+
+  return (
+    <React.Fragment>
+      {IsLoading && <MyLoadingSpinner />}
+      {!IsLoading && CategorySortedVideos && (
+        <div>
+          {" "}
           <div className="findVideoByCategory-container">
             {/* <div className="main-container"> */}
-              <div>
-                <VideoList items={CategorySortedVideos} />
-                <MoveToTopButton />
-              </div>
+            <div>
+              <VideoList items={CategorySortedVideos} />
+              <MoveToTopButton />
+            </div>
             {/* </div> */}
             {/* <div className="side-container"></div> */}
           </div>
-        )}
-      </React.Fragment>
-    );
-}
+        </div>
+      )}
+    </React.Fragment>
+  );
+};
 
 export default FindVideoByCategory;
