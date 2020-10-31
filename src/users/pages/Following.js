9874d2b4c383cd68ef1a04a3cfaf8b7c45143bfa
XX989tmx <issacgravity8@gmail.com>
2020-11-01 07:19:15 +0900

    adds my loading spinner to following js

--- src/users/pages/Following.js
+++ src/users/pages/Following.js
@@ -5,16 +5,19 @@ import FooterMainNavigation from "../../shared/components/Footer/FooterMainNavig
 import MyProfileSideNavigation from "../components/MyProfileSideNavigation";
 import FollowingList from "../components/FollowingList";
 import { useState } from "react";
-import {AuthContext} from "../../shared/context/auth-context";
+import { AuthContext } from "../../shared/context/auth-context";
 import { useContext } from "react";
 import { useEffect } from "react";
+import MyLoadingSpinner from "../../shared/components/UIElements/MyLoadingSpinner";
 
 const Following = () => {
   const auth = useContext(AuthContext);
   const [Following, setFollowing] = useState([]);
+  const [IsLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const fetch = async (params) => {
       async function getAndSetFollowingData(params) {
+        setIsLoading(true);
         let response;
         try {
           response = await Axios.get(
@@ -27,6 +30,11 @@ const Following = () => {
         console.log(data);
         const peopleYouAreFollowing = data.peopleYouAreFollowing;
         setFollowing(peopleYouAreFollowing);
+        function moveToTop(params) {
+          window.scrollTo(0, 0);
+        }
+        moveToTop();
+        setIsLoading(false);
       }
       await getAndSetFollowingData();
     };
@@ -34,34 +42,39 @@ const Following = () => {
   }, []);
   return (
     <React.Fragment>
-      {" "}
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          paddingLeft: "20%",
-        }}
-      >
-        <div
-          style={{ width: "1080px", minHeight: "100vh" }}
-          className="address-container"
-        >
-          <div>
-            {" "}
-            <h1> Following</h1>
-            <FollowingList Following={Following} />
+      {IsLoading && <MyLoadingSpinner />}{" "}
+      {!IsLoading && Following && (
+        <div>
+          {" "}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "wrap",
+              paddingLeft: "20%",
+            }}
+          >
+            <div
+              style={{ width: "1080px", minHeight: "100vh" }}
+              className="address-container"
+            >
+              <div>
+                {" "}
+                <h1> Following</h1>
+                <FollowingList Following={Following} />
+              </div>
+            </div>
+            <div
+              style={{ padding: "20px", width: "370px" }}
+              className="my-profile-sidebar-area"
+            >
+              <MyProfileSideNavigation />
+            </div>
           </div>
+          <MoveToTopButton />
+          <FooterMainNavigation />
         </div>
-        <div
-          style={{ padding: "20px", width: "370px" }}
-          className="my-profile-sidebar-area"
-        >
-          <MyProfileSideNavigation />
-        </div>
-      </div>
-      <MoveToTopButton />
-      <FooterMainNavigation />
+      )}
     </React.Fragment>
   );
 };
