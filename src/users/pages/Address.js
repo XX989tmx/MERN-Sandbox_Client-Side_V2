8aedafee0f4857ab2d7b9be432a573825ddecdac
XX989tmx <issacgravity8@gmail.com>
2020-11-01 05:55:16 +0900

    adds my loading spinner to address js

--- src/users/pages/Address.js
+++ src/users/pages/Address.js
@@ -7,13 +7,16 @@ import Axios from "axios";
 import { AuthContext } from "../../shared/context/auth-context";
 import AddressList from "../components/AddressList";
 import { Link } from "react-router-dom";
+import MyLoadingSpinner from "../../shared/components/UIElements/MyLoadingSpinner";
 
 const Address = () => {
   const auth = useContext(AuthContext);
   const [Addresses, setAddresses] = useState([]);
   const addressId = useParams().addressId;
+  const [IsLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const fetch = async (params) => {
+      setIsLoading(true);
       let response;
       try {
         response = await Axios.get(
@@ -26,55 +29,58 @@ const Address = () => {
       console.log(data);
       const addresses = data.user.addresses;
       setAddresses(addresses);
+      if (!!addresses) {
+        setIsLoading(false);
+      }
       console.log(addresses);
     };
     fetch();
   }, []);
 
-  
-
   return (
     <React.Fragment>
-      {" "}
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          paddingLeft: "20%",
-        }}
-      >
-        <div
-          style={{ width: "1080px", minHeight: "100vh" }}
-          className="address-container"
-        >
-          <div>
-            {" "}
-            <h1> Address</h1>
-            <h4>default address</h4>
-            <h4>check all address</h4>
-            <Link to={`/${auth.userId}/createAddress`}>
-              <h4>create address</h4>
-            </Link>
-            <h4>update address</h4>
-            <h4>delete address</h4>
-            <div>
-              <AddressList
-                Addresses={Addresses}
-                
-              />
+      {IsLoading && <MyLoadingSpinner />}{" "}
+      {!IsLoading && Addresses && (
+        <div>
+          {" "}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "wrap",
+              paddingLeft: "20%",
+            }}
+          >
+            <div
+              style={{ width: "1080px", minHeight: "100vh" }}
+              className="address-container"
+            >
+              <div>
+                {" "}
+                <h1> Address</h1>
+                <h4>default address</h4>
+                <h4>check all address</h4>
+                <Link to={`/${auth.userId}/createAddress`}>
+                  <h4>create address</h4>
+                </Link>
+                <h4>update address</h4>
+                <h4>delete address</h4>
+                <div>
+                  <AddressList Addresses={Addresses} />
+                </div>
+              </div>
+            </div>
+            <div
+              style={{ padding: "20px", width: "370px" }}
+              className="my-profile-sidebar-area"
+            >
+              <MyProfileSideNavigation />
             </div>
           </div>
+          <MoveToTopButton />
+          <FooterMainNavigation />
         </div>
-        <div
-          style={{ padding: "20px", width: "370px" }}
-          className="my-profile-sidebar-area"
-        >
-          <MyProfileSideNavigation />
-        </div>
-      </div>
-      <MoveToTopButton />
-      <FooterMainNavigation />
+      )}
     </React.Fragment>
   );
 };
