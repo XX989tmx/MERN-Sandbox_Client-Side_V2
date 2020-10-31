e81a8f6489e4a1613d2e9a1e19c92fc37463ea8a
XX989tmx <issacgravity8@gmail.com>
2020-11-01 06:15:25 +0900

    adds my loading spinner to tag index js

--- src/articles/pages/TagIndex.js
+++ src/articles/pages/TagIndex.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from "react";
 import { useHttpClient } from "../../shared/hooks/http-hook";
 import TagIndexList from "../components/TagIndexList";
+import MyLoadingSpinner from "../../shared/components/UIElements/MyLoadingSpinner";
 
 import "./TagIndex.css";
 import FooterMainNavigation from "../../shared/components/Footer/FooterMainNavigation";
@@ -10,15 +11,19 @@ import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 const TagIndex = () => {
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [TagIndexData, setTagIndexData] = useState([]);
+  const [IsLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const getTagIndex = async (params) => {
+      setIsLoading(true);
       try {
         const responseData = await sendRequest(
           process.env.REACT_APP_BACKEND_URL + `/articles/tagIndex`
         );
         // console.log(responseData);
         setTagIndexData(responseData.responseArray);
+
+        setIsLoading(false);
         // console.log(responseData.responseArray);
         // console.log(responseData.responseArray[0].tagName);
         // console.log(responseData.responseArray[0].count);
@@ -29,28 +34,27 @@ const TagIndex = () => {
 
   return (
     <React.Fragment>
-      {isLoading && (
-        <div className="center">
-          <LoadingSpinner />
+      {IsLoading && <MyLoadingSpinner />}
+      {!IsLoading && TagIndexData && (
+        <div>
+          {" "}
+          <div className="tag-index-container">
+            <div className="main-container">
+              <div>
+                <TagIndexList TagIndexData={TagIndexData} />
+              </div>
+              <div>
+                <span>
+                  <Link to={`/articles`}>Article Index</Link>
+                </span>
+              </div>
+            </div>
+
+            {/* <div className="side-container"></div> */}
+          </div>
+          <FooterMainNavigation />
         </div>
       )}
-
-      <div className="tag-index-container">
-        {!isLoading && TagIndexData && (
-          <div className="main-container">
-            <div>
-              <TagIndexList TagIndexData={TagIndexData} />
-            </div>
-            <div>
-              <span>
-                <Link to={`/articles`}>Article Index</Link>
-              </span>
-            </div>
-          </div>
-        )}
-        {/* <div className="side-container"></div> */}
-      </div>
-      <FooterMainNavigation />
     </React.Fragment>
   );
 };
