32a41a34f5f8bdba4988d995bb067f294e7a8a19
XX989tmx <issacgravity8@gmail.com>
2020-11-01 08:46:44 +0900

    adds my loading spinner to users js

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -3,58 +3,59 @@ import React, { useEffect, useState } from "react";
 import UsersList from "../components/UsersList";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
-import { useHttpClient } from '../../shared/hooks/http-hook';
+import { useHttpClient } from "../../shared/hooks/http-hook";
 import MoveToTopButton from "../../shared/components/UIElements/MoveToTopButton";
+import MyLoadingSpinner from "../../shared/components/UIElements/MyLoadingSpinner";
 
-import './Users.css';
+import "./Users.css";
 import FooterMainNavigation from "../../shared/components/Footer/FooterMainNavigation";
 
 const Users = () => {
-  const {isLoading, error, sendRequest, clearError} = useHttpClient();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [loadedUsers, setLoadedUsers] = useState();
-
+  const [IsLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const fetchUsers = async () => {
-      
+      setIsLoading(true);
       try {
-        const responseData = await sendRequest(process.env.REACT_APP_BACKEND_URL + "/users");
-
-        
+        const responseData = await sendRequest(
+          process.env.REACT_APP_BACKEND_URL + "/users"
+        );
 
         setLoadedUsers(responseData.users);
         console.log(responseData.users);
-      } catch (err) {
-      
+      } catch (err) {}
+      function moveToTop(params) {
+        window.scrollTo(0, 0);
       }
+      moveToTop();
+      setIsLoading(false);
     };
     fetchUsers();
   }, [sendRequest]);
 
- 
-
   return (
     <React.Fragment>
       <ErrorModal error={error} onClear={clearError} />
-      {isLoading && (
-        <div className="center">
-          <LoadingSpinner />
-        </div>
-      )}
-      {!isLoading && loadedUsers && (
-        <div className="users-container">
-          <div className="main-container">
-            <div className="post-form-area">
-              <p style={{ color: "grey", textAlign: "left" }}>
-                if something does not work, please reload the page.
-              </p>
-              <UsersList items={loadedUsers} />
-              <MoveToTopButton />
+      {IsLoading && <MyLoadingSpinner />}
+      {!IsLoading && loadedUsers && (
+        <div>
+          {" "}
+          <div className="users-container">
+            <div className="main-container">
+              <div className="post-form-area">
+                <p style={{ color: "grey", textAlign: "left" }}>
+                  if something does not work, please reload the page.
+                </p>
+                <UsersList items={loadedUsers} />
+              </div>
             </div>
+            {/* <div className="side-container"></div> */}
           </div>
-          {/* <div className="side-container"></div> */}
+          <MoveToTopButton />
+          <FooterMainNavigation />
         </div>
       )}
-      {!isLoading && loadedUsers && <FooterMainNavigation />}
     </React.Fragment>
   );
 };
