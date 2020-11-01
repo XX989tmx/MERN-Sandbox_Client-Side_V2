1c4059b0e41cd2627dd9b6b5d94622c6e480a881
XX989tmx <issacgravity8@gmail.com>
2020-11-01 09:08:33 +0900

    adds my loading spinner to crypto specific currency rating js

--- src/cryptos/pages/CryptoSpecificCurrencyRating.js
+++ src/cryptos/pages/CryptoSpecificCurrencyRating.js
@@ -1,39 +1,56 @@
-import Axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import CryptoSpecificCurrencyRatingList from '../components/CryptoSpecificCurrencyRatingList';
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
+import CryptoSpecificCurrencyRatingList from "../components/CryptoSpecificCurrencyRatingList";
 import { useParams, useHistory, Link } from "react-router-dom";
-import FooterMainNavigation from '../../shared/components/Footer/FooterMainNavigation';
-import MoveToTopButton from '../../shared/components/UIElements/MoveToTopButton';
+import FooterMainNavigation from "../../shared/components/Footer/FooterMainNavigation";
+import MoveToTopButton from "../../shared/components/UIElements/MoveToTopButton";
+import MyLoadingSpinner from "../../shared/components/UIElements/MyLoadingSpinner";
+
 const CryptoSpecificCurrencyRating = () => {
-    const queryName = useParams().queryName;
-    const [MatchedCurrencyInRating, setMatchedCurrencyInRating] = useState([]);
-    useEffect(() => {
-        const fetch = (params) => {
-            Axios.get(
-              process.env.REACT_APP_BACKEND_URL +
-                `/cryptos/currencies/${queryName}/ratings`
-            ).then((response) => {
-                console.log(response.data)
-                setMatchedCurrencyInRating(response.data.crypto);
-            }).catch((err) => {
-                
-            });
-        }
-        fetch();
-    }, []);
-    return (
-      <React.Fragment>
-        <div className="crypto-specific-container">
-          {/* <h3>Ratings</h3> */}
-          <CryptoSpecificCurrencyRatingList
-            MatchedCurrencyInRating={MatchedCurrencyInRating}
-          />
-          <div className="related-cryptocurrencies"></div>
+  const queryName = useParams().queryName;
+  const [MatchedCurrencyInRating, setMatchedCurrencyInRating] = useState([]);
+  const [IsLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    setIsLoading(true);
+    const fetch = (params) => {
+      Axios.get(
+        process.env.REACT_APP_BACKEND_URL +
+          `/cryptos/currencies/${queryName}/ratings`
+      )
+        .then((response) => {
+          console.log(response.data);
+          setMatchedCurrencyInRating(response.data.crypto);
+          function moveToTop(params) {
+            window.scrollTo(0, 0);
+          }
+          moveToTop();
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    fetch();
+  }, []);
+  return (
+    <React.Fragment>
+      {IsLoading && <MyLoadingSpinner />}
+      {!IsLoading && MatchedCurrencyInRating && (
+        <div>
+          {" "}
+          <div className="crypto-specific-container">
+            {/* <h3>Ratings</h3> */}
+            <CryptoSpecificCurrencyRatingList
+              MatchedCurrencyInRating={MatchedCurrencyInRating}
+            />
+            <div className="related-cryptocurrencies"></div>
+          </div>
+          <MoveToTopButton />
+          <FooterMainNavigation />
         </div>
-        <MoveToTopButton />
-        <FooterMainNavigation />
-      </React.Fragment>
-    );
-}
+      )}
+    </React.Fragment>
+  );
+};
 
 export default CryptoSpecificCurrencyRating;
