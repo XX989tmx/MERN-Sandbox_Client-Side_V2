22e96d4d771fd79581df628431e6b205622acc40
XX989tmx <issacgravity8@gmail.com>
2020-11-01 09:01:11 +0900

    adds my loading spinner to crypto specific currency historical data js

--- src/cryptos/pages/CryptoSpecificCurrencyHistoricalData.js
+++ src/cryptos/pages/CryptoSpecificCurrencyHistoricalData.js
@@ -1,38 +1,54 @@
-import Axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import CryptoSpecificCurrencyHistoricalDataList from '../components/CryptoSpecificCurrencyHistoricalDataList';
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
+import CryptoSpecificCurrencyHistoricalDataList from "../components/CryptoSpecificCurrencyHistoricalDataList";
 import { useParams, useHistory, Link } from "react-router-dom";
-import './CryptoSpecificCurrencyHistoricalData.css';
-import MoveToTopButton from '../../shared/components/UIElements/MoveToTopButton';
-import FooterMainNavigation from '../../shared/components/Footer/FooterMainNavigation';
+import MyLoadingSpinner from "../../shared/components/UIElements/MyLoadingSpinner";
+import "./CryptoSpecificCurrencyHistoricalData.css";
+import MoveToTopButton from "../../shared/components/UIElements/MoveToTopButton";
+import FooterMainNavigation from "../../shared/components/Footer/FooterMainNavigation";
 const CryptoSpecificCurrencyHistoricalData = () => {
-    const queryName = useParams().queryName;
-    const [CryptoData, setCryptoData] = useState([]);
+  const queryName = useParams().queryName;
+  const [CryptoData, setCryptoData] = useState([]);
+  const [IsLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        const fetch = (params) => {
-            Axios.get(
-              process.env.REACT_APP_BACKEND_URL +
-                `/cryptos/currencies/${queryName}/historicalData`
-            )
-              .then((response) => {
-                console.log(response.data);
-                setCryptoData(response.data.crypto);
-              })
-              .catch((err) => {console.log(err);});
-        };
-        fetch();
-    }, []);
-    return (
-      <React.Fragment>
-        <div className="crypto-specific-historical-data-container">
-          <CryptoSpecificCurrencyHistoricalDataList CryptoData={CryptoData} />
-          <div className="related-cryptocurrencies"></div>
+  useEffect(() => {
+    setIsLoading(true);
+    const fetch = (params) => {
+      Axios.get(
+        process.env.REACT_APP_BACKEND_URL +
+          `/cryptos/currencies/${queryName}/historicalData`
+      )
+        .then((response) => {
+          console.log(response.data);
+          setCryptoData(response.data.crypto);
+          function moveToTop(params) {
+            window.scrollTo(0, 0);
+          }
+          moveToTop();
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    fetch();
+  }, []);
+  return (
+    <React.Fragment>
+      {IsLoading && <MyLoadingSpinner />}
+      {!IsLoading && CryptoData && (
+        <div>
+          {" "}
+          <div className="crypto-specific-historical-data-container">
+            <CryptoSpecificCurrencyHistoricalDataList CryptoData={CryptoData} />
+            <div className="related-cryptocurrencies"></div>
+          </div>
+          <MoveToTopButton />
+          <FooterMainNavigation />
         </div>
-        <MoveToTopButton />
-        <FooterMainNavigation />
-      </React.Fragment>
-    );
-}
+      )}
+    </React.Fragment>
+  );
+};
 
 export default CryptoSpecificCurrencyHistoricalData;
