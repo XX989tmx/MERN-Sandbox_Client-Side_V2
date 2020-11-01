ca2c3676a25da0cd80e4d2bfc154ae94b959b130
XX989tmx <issacgravity8@gmail.com>
2020-11-01 10:04:18 +0900

    adds flex center style to auth card

--- src/users/pages/Auth.js
+++ src/users/pages/Auth.js
@@ -114,60 +114,74 @@ const Auth = (params) => {
   return (
     <React.Fragment>
       <ErrorModal error={error} onClear={clearError} />
-      
-        {isLoading && <LoadingSpinner asOverlay />}
-        <div className="auth-container"><Card className="authentication">
-          <h2 className="center">Login Required</h2>
-          <hr />
-          <form onSubmit={authSubmitHandler}>
-            {!isLoginMode && (
+
+      {isLoading && <LoadingSpinner asOverlay />}
+      <div className="auth-background-area">
+        {" "}
+        <div
+          className="auth-container"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Card
+            className="authentication"
+            style={{ width: "430px", height: "780px" }}
+          >
+            <h2 className="center">Login Required</h2>
+            <hr />
+            <form onSubmit={authSubmitHandler}>
+              {!isLoginMode && (
+                <Input
+                  element="input"
+                  id="name"
+                  type="text"
+                  label="Your Name"
+                  validators={[VALIDATOR_REQUIRE()]}
+                  errorText="Please enter a name"
+                  onInput={inputHandler}
+                />
+              )}
+
+              {!isLoginMode && (
+                <ImageUpload
+                  center
+                  id="image"
+                  onInput={inputHandler}
+                  errorText="Please provide an image."
+                />
+              )}
+
               <Input
                 element="input"
-                id="name"
-                type="text"
-                label="Your Name"
-                validators={[VALIDATOR_REQUIRE()]}
-                errorText="Please enter a name"
+                id="email"
+                type="email"
+                label="E-Mail"
+                validators={[VALIDATOR_EMAIL()]}
+                errorText="Please enter a valid email address"
                 onInput={inputHandler}
               />
-            )}
-
-            {!isLoginMode && (
-              <ImageUpload
-                center
-                id="image"
+              <Input
+                element="input"
+                id="password"
+                type="password"
+                label="Password"
+                validators={[VALIDATOR_MINLENGTH(6)]}
+                errorText="Please enter a valid password, at least 6 characters."
                 onInput={inputHandler}
-                errorText="Please provide an image."
               />
-            )}
-
-            <Input
-              element="input"
-              id="email"
-              type="email"
-              label="E-Mail"
-              validators={[VALIDATOR_EMAIL()]}
-              errorText="Please enter a valid email address"
-              onInput={inputHandler}
-            />
-            <Input
-              element="input"
-              id="password"
-              type="password"
-              label="Password"
-              validators={[VALIDATOR_MINLENGTH(6)]}
-              errorText="Please enter a valid password, at least 6 characters."
-              onInput={inputHandler}
-            />
-            <Button btnBlack type="submit" disabled={!formState.isValid}>
-              {isLoginMode ? "LOGIN" : "SIGNUP"}
+              <Button btnBlack type="submit" disabled={!formState.isValid}>
+                {isLoginMode ? "LOGIN" : "SIGNUP"}
+              </Button>
+            </form>
+            <Button btnBlackInverse onClick={switchModeHandler}>
+              SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}
             </Button>
-          </form>
-          <Button btnBlackInverse onClick={switchModeHandler}>
-            SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}
-          </Button></Card>
+          </Card>
         </div>
-      
+      </div>
     </React.Fragment>
   );
 };
